81a57d3fca7c06461d35a7894a44e40766e2b95b
johanattia <attia.johan@gmail.com>
2022-06-15 23:57:26 +0200

    add get_input_shape function for input image

--- src/dm/denoising_diffusion_probabilistic_models/utils.py
+++ src/dm/denoising_diffusion_probabilistic_models/utils.py
@@ -12,3 +12,12 @@ def clone_initializer(initializer: tf.keras.initializers.Initializer):
     ):
         return initializer.__class__.from_config(initializer.get_config())
     return initializer
+
+
+def get_input_shape(input_shape: tf.TensorShape):
+    if len(input_shape) == 4:
+        return tf.TensorShape(input_shape)[1:]
+    elif len(input_shape) == 3:
+        return tf.TensorShape(input_shape)
+    else:
+        raise ValueError("Input shape must be (B, H, W, C) or (B, C, H, W)")
