b98f2a51aee27a9574e8e0ffe990ec097afc5d21
johanattia <attia.johan@gmail.com>
2022-06-15 23:58:57 +0200

    initiate DiffusionUNet

--- src/dm/denoising_diffusion_probabilistic_models/unet.py
+++ src/dm/denoising_diffusion_probabilistic_models/unet.py
@@ -1,8 +1,13 @@
-"""U-Net with TensorFlow 2.x based on https://github.com/hojonathanho/diffusion/blob/master/diffusion_tf/models/unet.py"""
+"""U-Net Diffusion Model from https://github.com/hojonathanho/diffusion/blob/master/diffusion_tf/models/unet.py"""
 
 
 import tensorflow as tf
+from .model import DiffusionModel
 
 
 class UNet(tf.keras.Model):
+    pass
+
+
+class DiffusionUNet(DiffusionModel):
     pass
