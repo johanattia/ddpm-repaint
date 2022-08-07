1197d133eda926010ec4f69a4661e1097c8838b1
johanattia <attia.johan@gmail.com>
2022-08-08 00:40:24 +0200

    initiate callbacks: image synthesis

---
+++ src/dm/denoising_diffusion_probabilistic_models/callbacks.py
@@ -0,0 +1,76 @@
+"""Useful ImageGenerationCallback for training.
+Reference: https://keras.io/examples/generative/gaugan/#gan-monitor-callback
+"""
+
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+
+from .utils import get_input_shape
+
+
+class DiffusionSynthesisCallback(tf.keras.callbacks.Callback):
+    def __init__(
+        self,
+        image_rows: int = 4,
+        image_cols: int = 4,
+        image_width: int = 4,
+        image_height: int = 4,
+        epoch_interval: int = 5,
+        verbose: int = 0,
+    ):
+        super().__init__()
+        self.image_rows = image_rows
+        self.image_cols = image_cols
+        self.sampling_size = image_rows * image_cols
+        self.image_width = image_width
+        self.image_height = image_height
+        self.epoch_interval = epoch_interval
+        self.verbose = verbose
+
+    def on_epoch_end(self, epoch: int):
+        if epoch % self.epoch_interval == 0:
+            images = self.model.sampling(
+                sampling_size=self.sampling_size, verbose=self.verbose
+            )
+            self.imshow(images)
+
+    def imshow(
+        self,
+        images: tf.Tensor = None,
+        input_shape: tf.TensorShape = None,
+        data_format: str = "channels_last",
+    ):
+        if self.model is not None:
+            input_shape, data_format = self.model.input_shape, self.model.data_format
+        elif data_format is None:
+            data_format = "channels_last"
+        elif (input_shape is None) or (
+            data_format not in ["channels_last", "channels_first"]
+        ):
+            raise ValueError(
+                """`data_format` must value `channels_last` or `channels_first`. 
+                Default behavior to `channels_last`.
+                """
+            )
+        else:
+            input_shape = get_input_shape(input_shape)
+
+        if images is None:
+            images = tf.random.normal([self.sampling_size] + input_shape.as_list())
+        fig = plt.figure(
+            figsize=(
+                self.image_cols * self.self.image_width,
+                self.image_rows * self.image_height,
+            )
+        )
+        for i in range(self.sampling_size):
+            plt.subplot(self.image_rows, self.image_cols, i + 1)
+            plt.imshow(
+                tf.keras.preprocessing.image.array_to_img(
+                    images[i], data_format=data_format
+                )
+            )
+            plt.axis("off")
+        plt.show()
