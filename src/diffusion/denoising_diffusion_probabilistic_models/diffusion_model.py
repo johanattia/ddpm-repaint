adda37dd91261779f574fb01b627bd0e1e3de8e2
johanattia <attia.johan@gmail.com>
2022-08-08 17:16:08 +0200

    slight reformatting

--- src/diffusion/denoising_diffusion_probabilistic_models/diffusion_model.py
+++ src/diffusion/denoising_diffusion_probabilistic_models/diffusion_model.py
@@ -91,6 +91,7 @@ class DiffusionModel(tf.keras.Model):
 
             progbar.update(step, finalize=False)
             step.assign_add(1)
+
         progbar.update(step, finalize=True)
 
         return x
