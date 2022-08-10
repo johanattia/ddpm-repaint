9f735854becf74ed88ae7e25ca2771a09b2fb4f2
johanattia <attia.johan@gmail.com>
2022-08-10 22:49:44 +0200

    implement ConvBlock & ResidualBlock

--- src/diffusion/ddpm/layers.py
+++ src/diffusion/ddpm/layers.py
@@ -1,8 +1,8 @@
-"""Layers needed for U-Net & PixelCNN++ models"""
+"""U-Net layers"""
 
 
-from multiprocessing.sharedctypes import Value
 from typing import Callable, Dict, Tuple, Union
+from matplotlib import units
 
 import tensorflow as tf
 import tensorflow_addons as tfa
@@ -10,9 +10,11 @@ import tensorflow_addons as tfa
 from utils import clone_initializer
 
 
-def SinusoidalPositionEmbedding(embed_dim: int) -> tf.Tensor:
+def PositionEmbedding(embed_dim: int):
     # Reference: https://github.com/hojonathanho/diffusion/blob/master/diffusion_tf/nn.py#L90-L109
-    def sinusoidal_embedding(steps: tf.Tensor) -> tf.Tensor:
+    def _sinusoidal_embedding(
+        steps: tfa.types.TensorLike,
+    ) -> tfa.types.FloatTensorLike:
         # input shape: (B, 1)
         batch_size = tf.shape(steps)[0]
         if steps.shape != tf.TensorShape([batch_size, 1]):
@@ -31,128 +33,233 @@ def SinusoidalPositionEmbedding(embed_dim: int) -> tf.Tensor:
 
         return step_embed  # output shape: (B, embed_dim)
 
-    return tf.keras.layers.Lambda(sinusoidal_embedding)
+    return tf.keras.layers.Lambda(_sinusoidal_embedding)
 
 
 class CELU(tf.keras.layers.Layer):
     def __init__(self, alpha: float = 1.0, axis: int = -1, **kwargs):
         super().__init__(**kwargs)
-        self.axis = axis
-        self.alpha = float(alpha)
-        self.concat = tf.keras.layers.Concatenate(axis=self.axis)
-        self.elu = tf.keras.layers.Elu(alpha=self.alpha)
+        if axis is None:
+            raise ValueError(
+                "`axis` of a CELU layer cannot be None, expecting an integer."
+            )
+        self._axis = axis
+        self._alpha = float(alpha)
 
-    def call(self, inputs: tf.Tensor) -> tf.Tensor:
+        self.elu = tf.keras.layers.Elu(alpha=self._alpha)
+        self.concat = tf.keras.layers.Concatenate(axis=self._axis)
+
+    def call(self, inputs: tfa.types.FloatTensorLike) -> tfa.types.FloatTensorLike:
         return self.elu(self.concat([inputs, -inputs]))
+
+    def get_config(self) -> Dict:
+        config = super(CELU, self).get_config()
+        config.update({"alpha": self._alpha, "axis": self._axis})
+        return config
+
+
+class ConvBlock(tf.keras.layers.Layer):
+    def __init__(
+        self,
+        filters: int = None,
+        kernel_size: Union[int, Tuple[int]] = (3, 3),
+        strides: Tuple[int] = (1, 1),
+        data_format: str = "channels_last",
+        dilation_rate: Tuple[int] = (1, 1),
+        use_bias: bool = True,
+        dropout: float = 0.2,
+        init_scale: float = 1.0,
+        **kwargs,
+    ):
+        self.name = kwargs.pop("name", "ConvBlock")
+        super().__init__(**kwargs)
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.data_format = data_format
+        self.dilation_rate = dilation_rate
+        self.use_bias = use_bias
+        self._dropout = dropout
+        self._init_scale = init_scale
+
+    def build(self, input_shape: tf.TensorShape):
+        input_shape = tf.TensorShape(input_shape)
+        if len(input_shape) != 4:
+            raise ValueError(
+                f"""Input of a `ConvBlock` layer should correspond to a batch of images.
+                Expected shape is either (B, H, W, C) or (B, C, H, W).  Received: {input_shape}.
+                """
+            )
+
+        if self.data_format == "channels_last":  # (B, H, W, C)
+            channels = input_shape[-1]
+        else:  # (B, C, H, W)
+            channels = input_shape[1]
+
+        if self.filters is None:
+            self.filters = channels
+
+        if self._dropout:
+            self.dropout = tf.keras.layers.Dropout(rate=self._dropout)
+
+        self.conv = tf.keras.layers.Conv2D(
+            filters=self.filters,
+            kernel_size=self.kernel_size,
+            strides=self.strides,
+            padding="same",
+            data_format=self.data_format,
+            dilation_rate=self.dilation_rate,
+            use_bias=self.use_bias,
+            kernel_initializer=tf.keras.initializers.VarianceScaling(
+                scale=self._init_scale, mode="fan_avg", distribution="uniform"
+            ),
+        )
+        self.normalize = tfa.layers.GroupNormalization()
+        super().build(input_shape)
+
+    def call(
+        self, inputs: tfa.types.FloatTensorLike, training: bool = None
+    ) -> tf.Tensor:
+        x = tf.nn.silu(self.normalize(inputs))
+
+        if self._dropout:
+            x = self.dropout(x, training=training)
+
+        return self.conv(x)
+
+    def get_config(self) -> Dict:
+        config = super(ConvBlock, self).get_config()
+        config.update(
+            {
+                "filters": self.filters,
+                "kernel_size": self.kernel_size,
+                "strides": self.strides,
+                "data_format": self.data_format,
+                "dilation_rate": self.dilation_rate,
+                "use_bias": self.use_bias,
+                "dropout": self._dropout,
+                "init_scale": self._init_scale,
+            }
+        )
+        return config
 
 
 class ResidualBlock(tf.keras.layers.Layer):
     def __init__(
         self,
-        filters: int,
         output_channel: int = None,
-        conv_shortcut: bool = False,
         kernel_size: Union[int, Tuple[int]] = (3, 3),
         strides: Tuple[int] = (1, 1),
-        padding: str = "valid",
         data_format: str = "channels_last",
         dilation_rate: Tuple[int] = (1, 1),
-        groups: int = 1,
-        activation: Callable = tf.nn.silu,  # or tf.keras.activations.swish
-        use_bias: bool = True,
+        conv_shortcut: bool = False,
         dropout: float = 0.2,
-        kernel_initializer: Union[str, Callable] = "glorot_uniform",
-        bias_initializer: Union[str, Callable] = "zeros",
-        kernel_regularizer: Union[str, Callable] = None,
-        bias_regularizer: Union[str, Callable] = None,
-        activity_regularizer: Union[str, Callable] = None,
-        kernel_constraint: Union[str, Callable] = None,
-        bias_constraint: Union[str, Callable] = None,
         **kwargs,
     ):
-        self.name = kwargs.pop("name", "residual_block")
+        self.name = kwargs.pop("name", "ResidualBlock")
         super().__init__(**kwargs)
-        self._output_channel = output_channel
-        self._conv_shortcut = conv_shortcut
-        self.filters = filters
+        self.output_channel = output_channel
         self.kernel_size = kernel_size
         self.strides = strides
-        self.padding = padding
         self.data_format = data_format
-        self._dropout = dropout
         self.dilation_rate = dilation_rate
-        self.groups = groups
-        self.activation = activation
-        self.use_bias = use_bias
-        self._kernel_initializer = tf.keras.initializers.get(kernel_initializer)
-        self._bias_initializer = tf.keras.initializers.get(bias_initializer)
-        self._kernel_regularizer = tf.keras.regularizers.get(kernel_regularizer)
-        self._bias_regularizer = tf.keras.regularizers.get(bias_regularizer)
-        self._activity_regularizer = tf.keras.regularizers.get(activity_regularizer)
-        self._kernel_constraint = tf.keras.constraints.get(kernel_constraint)
-        self._bias_constraint = tf.keras.constraints.get(bias_constraint)
-
-        self.common_weights_params = dict(
-            kernel_regularizer=self._kernel_regularizer,
-            bias_regularizer=self._bias_regularizer,
-            activity_regularizer=self._activity_regularizer,
-            kernel_constraint=self._kernel_constraint,
-            bias_constraint=self._bias_constraint,
-        )
+        self.conv_shortcut = conv_shortcut
+        self.dropout = dropout
 
     def build(self, input_shape: tf.TensorShape):
         input_shape = tf.TensorShape(input_shape)
+
         if self.data_format == "channels_last":  # (B, H, W, C)
             channels = input_shape[-1]
         else:  # (B, C, H, W)
             channels = input_shape[1]
-        if self._output_channel is None:
-            self._output_channel = channels
 
-        self.group_normalization1 = tfa.layers.GroupNormalization()
-        self.group_normalization2 = tfa.layers.GroupNormalization()
-        self.dropout = tf.keras.layers.Dropout(rate=self._dropout)
+        if self.output_channel is None:
+            self.output_channel = channels
 
-        self.conv_layers = [
-            tf.keras.layers.Conv2D(
-                filters=self._output_channel,
-                kernel_size=self.kernel_size,
-                strides=self.strides,
-                padding=self.padding,
-                data_format=self.data_format,
-                dilation_rate=self.dilation_rate,
-                groups=self.groups,
-                activation=self.activation,
-                use_bias=self.use_bias,
-                kernel_initializer=clone_initializer(self._kernel_initializer),
-                bias_initializer=clone_initializer(self._bias_initializer),
-                **self.common_weights_params,
-                name=f"{self.name}_{i}",
-            )
-            for i in range(2)
-        ]
-        if self._output_channel:
-            self.conv_layers.append(
-                tf.keras.layers.Conv2D(
-                    filters=self._output_channel,
+        self.conv_block1 = ConvBlock(
+            filters=self.output_channel,
+            kernel_size=self.kernel_size,
+            strides=self.strides,
+            data_format=self.data_format,
+            dilation_rate=self.dilation_rate,
+            dropout=None,
+        )
+        self.conv_block2 = ConvBlock(
+            filters=self.output_channel,
+            kernel_size=self.kernel_size,
+            strides=self.strides,
+            data_format=self.data_format,
+            dilation_rate=self.dilation_rate,
+            dropout=self.dropout,
+            init_scale=1e-10,
+        )
+        self.dense = tf.keras.layers.Dense(
+            units=self.output_channel,
+            use_bias=True,
+            kernel_initializer=tf.keras.initializers.VarianceScaling(
+                scale=1.0, mode="fan_avg", distribution="uniform"
+            ),
+        )
+
+        # Projection of input tensor in output space before residual addition
+        self.output_projection = None
+        if self.output_channel != channels:
+            if self.conv_shortcut:
+                self.output_projection = tf.keras.layers.Conv2D(
+                    filters=self.output_channel,
                     kernel_size=self.kernel_size,
                     strides=self.strides,
-                    padding=self.padding,
+                    padding="same",
                     data_format=self.data_format,
                     dilation_rate=self.dilation_rate,
-                    groups=self.groups,
-                    activation=self.activation,
-                    use_bias=self.use_bias,
-                    kernel_initializer=clone_initializer(self._kernel_initializer),
-                    bias_initializer=clone_initializer(self._bias_initializer),
-                    **self.common_weights_params,
-                    name=f"{self.name}_{2}",
+                    use_bias=True,
+                    kernel_initializer=tf.keras.initializers.VarianceScaling(
+                        scale=1.0, mode="fan_avg", distribution="uniform"
+                    ),
                 )
-            )
-        else:
-            pass  # einsum ops
+            else:
+                self.output_projection = tf.keras.layers.experimental.EinsumDense(
+                    "abcd,de->abce",
+                    output_shape=(None, None, self.output_channel),
+                    bias_axes="e",
+                )
 
         super().build(input_shape)
 
-    def call(self, inputs: tf.Tensor, training: bool = None) -> tf.Tensor:
-        pass
+    def call(
+        self,
+        inputs: Tuple[tfa.types.FloatTensorLike, tfa.types.TensorLike],
+        training: bool = None,
+    ) -> tf.Tensor:
+        x, step_embed = inputs
+
+        h = self.conv_block1(x)
+        h += self.dense(tf.nn.silu(step_embed))[:, tf.newaxis, tf.newaxis, :]
+        h = self.conv_block2(x, training=training)
+
+        if self.output_projection is not None:
+            if self.data_format == "channels_first" and not self.conv_shortcut:
+                x = tf.einsum("abcd->adbc", x)
+            x = self.output_projection(x)
+
+        return x + h
+
+    def get_config(self) -> Dict:
+        config = super(ResidualBlock, self).get_config()
+        config.update(
+            {
+                "output_channel": self.output_channel,
+                "kernel_size": self.kernel_size,
+                "strides": self.strides,
+                "data_format": self.data_format,
+                "dilation_rate": self.dilation_rate,
+                "conv_shortcut": self.conv_shortcut,
+                "dropout": self.dropout,
+            }
+        )
+        return config
+
+
+class AttentionBlock(tf.keras.layers.Layer):
+    pass
