5576876472dcfa802216894dfc73752bbdc85e8e
johanattia <attia.johan@gmail.com>
2022-11-10 21:56:59 +0100

    update CELU layer: format+solve ELU call

--- src/ddpm/layers.py
+++ src/ddpm/layers.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 from tensorflow.keras import layers
 
 import tensorflow_addons as tfa
-from tensorflow_addons import types # import FloatTensorLike, TensorLike
+from tensorflow_addons import types  # import FloatTensorLike, TensorLike
 
 
 # TODO:
@@ -48,18 +48,17 @@ class CELU(layers.Layer):
             raise ValueError(
                 "`axis` of a CELU layer cannot be None, expecting an integer."
             )
-        self._axis = axis
-        self._alpha = float(alpha)
-
-        self.elu = layers.Elu(alpha=self._alpha)
-        self.concat = layers.Concatenate(axis=self._axis)
+        self.axis = axis
+        self.alpha = float(alpha)
+        self.elu = layers.ELU(alpha=self.alpha)
+        self.concat = layers.Concatenate(axis=self.axis)
 
     def call(self, inputs: types.FloatTensorLike) -> types.FloatTensorLike:
         return self.elu(self.concat([inputs, -inputs]))
 
     def get_config(self) -> Dict:
         config = super(CELU, self).get_config()
-        config.update({"alpha": self._alpha, "axis": self._axis})
+        config.update({"alpha": self.alpha, "axis": self.axis})
         return config
 
 
